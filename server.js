59d605c31d50ab79a552a568121eab9cec0f99b4
Jaehwajh <jaehwa.jh@gmail.com>
2022-12-27 14:41:35 -0800

    server backend

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const mongoose  = requrie("mongoose");
+const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const methodOverride = require("method-override");
+const flash = require("express-flash");
+const logger = require("morgan");
+const connectDB = require("./config/database");
+
