8ec9790bb91e846216385bf5c83e1f3679e04fb4
Jaehwajh <jaehwa.jh@gmail.com>
2022-12-28 17:16:50 -0800

    added auth controller, sign-in, sign-up, and logout

--- routes/main.js
+++ routes/main.js
@@ -2,8 +2,17 @@ const express = require("express");
 const router = express.Router();
 const {ensureAuth, ensureGuest} = require("..middleware/auth");
 const homepageController = require("../controllers/homepage");
+const authController = require("../controllers/auth");
 
 // Loading Homepage
 router.get("/", homepageController.getHomepage);
+
+router.get("/login", authController.getLogin);
+router.post("/login", authController.postLogin);
+
+router.get("/logout", authController.logout);
+
+router.get("/signup", authController.getSignup);
+router.post("/signup", authController.postSignup);
 
 module.exports = router;
