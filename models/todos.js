66b5e3f1f2f7e0cf3dc6226a5b0eba67e8f53e67
Jaehwajh <jaehwa.jh@gmail.com>
2023-02-03 11:46:30 -0800

    began implementation for todo list support for dashboard

---
+++ models/todos.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const todoSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    todo: {
+        type: String,
+        required: true
+    },
+});
+
+module.exports = mongoose.model("Todo", todoSchema);
